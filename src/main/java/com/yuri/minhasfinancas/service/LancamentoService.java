640becaf1378c680897b4d1e2c33b22c7ce26c41
Yuri Breion <yuribreion@br.ibm.com>
2021-12-07 06:35:48 -0300

    Adding feature to query lancamentos by ID

--- src/main/java/com/yuri/minhasfinancas/service/LancamentoService.java
+++ src/main/java/com/yuri/minhasfinancas/service/LancamentoService.java
@@ -4,6 +4,7 @@ import com.yuri.minhasfinancas.model.entity.Lancamento;
 import com.yuri.minhasfinancas.model.enums.StatusLancamento;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LancamentoService {
 
@@ -18,4 +19,6 @@ public interface LancamentoService {
     List<Lancamento> buscar(Lancamento lancamentoFiltro);
 
     void validar(Lancamento lancamento);
+
+    Optional<Lancamento> pegarLancamentoPorId(Long id);
 }
